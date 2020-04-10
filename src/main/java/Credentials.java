f1b124023eff7d2be2f9c1f1ced68b8d23b23923
Steven Ngo <61873590+ngotunhien@users.noreply.github.com>
2020-04-10 10:15:53 -0400

    Add files via upload

---
+++ src/main/java/Credentials.java
@@ -0,0 +1,14 @@
+/**
+ * Created by mattua on 23/07/2016.
+ */
+public class Credentials {
+
+
+    public static final String access_key_id= "AKIAV37PJUHOPJ4NPSPH";
+
+    public static final String secret_access_key ="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";
+
+
+
+}
+
