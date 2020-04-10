a3afb4ad67f1aadca5794ea26e2cf9761bfe1d36
Steven Ngo <61873590+ngotunhien@users.noreply.github.com>
2020-04-10 10:17:17 -0400

    Update Credentials.java

--- src/main/java/Credentials.java
+++ src/main/java/Credentials.java
@@ -1,14 +1,8 @@
-/**
- * Created by mattua on 23/07/2016.
+/*
+Reference from mattua github
  */
 public class Credentials {
-
-
-    public static final String access_key_id= "AKIAV37PJUHOPJ4NPSPH";
-
-    public static final String secret_access_key ="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";
-
-
-
+    public static final String access_key_id= "xxxxxxxxxx";
+    public static final String secret_access_key ="yyyyyyyyyyyyyy";
 }
 
